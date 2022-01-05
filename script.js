ec8cb2fb53c5f709fbd31dc2ce30b301416ebc22
StrikerEureka27 <pablocaceros@gmail.com>
2022-01-04 23:50:55 -0600

    Se cambio el estilo y se agrego la eliminacion de las notas

--- script.js
+++ script.js
@@ -4,18 +4,16 @@ var tasklist = [];
 const addbtn = document.getElementById("add");
 const deletebtn = document.getElementById("delete");
 const listtemplate = document.getElementById("li-item");
-const elementli = document.getElementById("el-li");
 
 
 const generateDate = (date) => {
     let format = {
         dd: date.getDay() + 2,
-        mm: date.getMonth() + 1,
+        mm: date.getMonth() + 1, 
         yy: date.getFullYear()
     }
     return `📅 ${format.dd}/${format.mm}/${format.yy} ⏰ ${date.getUTCHours()}:${date.getMinutes()}`;
 }
-
 
 addbtn.addEventListener("click", function () {
     let inputnote = document.getElementById("note").value;
@@ -23,19 +21,16 @@ addbtn.addEventListener("click", function () {
     const date = new Date();
     tasklist.push({ id: tasklist.length, note: inputnote });
     li.innerHTML = `    
-        <li id="el-li" ><span>${generateDate(date)}</span> 📝 ${tasklist[tasklist.length - 1].note} value="${tasklist[tasklist.length - 1].id}"
-        <button id="delete" class="delete_btn" onClick="deleteItem(${tasklist[tasklist.length - 1].id})" >Delete</button> </li>     
-    `;
-    console.log(tasklist);
+        <li id="${tasklist[tasklist.length - 1].id}"><span>${generateDate(date)}</span> 📝 ${tasklist[tasklist.length - 1].note}
+        <button id="delete" class="delete_btn" onClick="deleteItem(${tasklist[tasklist.length - 1].id})" >Delete</button> </li>`;
     document.getElementById("note").value = null; 
     return listtemplate.appendChild(li);
 });
 
 const deleteItem = (id) => {
-    console.log("Se elimino un item...");
+    const selectedElement = document.getElementById(`${id}`);    
     tasklist.splice(id, 1);
-    document.remove(li);
-    console.log(tasklist);
+    return selectedElement.remove();     
 }
 
 

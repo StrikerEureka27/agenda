f8110c1e7695beaae587deb3af3f7f40feb3ebc6
StrikerEureka27 <pablocaceros@gmail.com>
2022-01-05 22:29:02 -0600

    Se cambio el estilo de la aplicacion

--- script.js
+++ script.js
@@ -12,17 +12,41 @@ const generateDate = (date) => {
         mm: date.getMonth() + 1, 
         yy: date.getFullYear()
     }
-    return `📅 ${format.dd}/${format.mm}/${format.yy} ⏰ ${date.getUTCHours()}:${date.getMinutes()}`;
+    return `${format.dd}/${format.mm}/${format.yy}`;
 }
+
+const generateHour = (date) => {
+    let format = {
+        dd: date.getDay() + 2,
+        mm: date.getMonth() + 1, 
+        yy: date.getFullYear()
+    }
+    return `${date.getHours()}:${date.getMinutes()}`;
+}
+
+
 
 addbtn.addEventListener("click", function () {
     let inputnote = document.getElementById("note").value;
     const li = document.createElement("div");
     const date = new Date();
     tasklist.push({ id: tasklist.length, note: inputnote });
-    li.innerHTML = `    
-        <li id="${tasklist[tasklist.length - 1].id}"><span>${generateDate(date)}</span> 📝 ${tasklist[tasklist.length - 1].note}
-        <button id="delete" class="delete_btn" onClick="deleteItem(${tasklist[tasklist.length - 1].id})" >Delete</button> </li>`;
+    li.innerHTML = `   
+        <li id="${tasklist[tasklist.length - 1].id}" style="margin: 0px; padding: 0px; width: 100%; height: 60px; display: block;" >
+                    <div class="infoitem" >
+                        <ul>
+                            <li> 🕑 ${generateHour(date)} </li>
+                            <li> 📅 ${generateDate(date)} </li>
+                        </ul>   
+                    </div>
+                    <div class="contentnote"  >
+                        <p >${tasklist[tasklist.length - 1].note}</p>
+                    </div>
+                    <button id="delete" onClick="deleteItem(${tasklist[tasklist.length - 1].id})"  >
+                        Delete
+                    </button>
+                </li>
+        `;
     document.getElementById("note").value = null; 
     return listtemplate.appendChild(li);
 });
